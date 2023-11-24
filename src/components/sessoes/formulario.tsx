0a7f5ee4a633df48b3f8dd983b69d87bbe37487f
unknown <brunabaldissera12@gmail.com>
2023-11-24 17:51:47 -0300

    mensagem erro preenchimento

--- src/components/sessoes/formulario.tsx
+++ src/components/sessoes/formulario.tsx
@@ -1,6 +1,6 @@
+import React, { useState } from "react";
 import Sessao from "@/core/Sessao";
 import Entrada from "./entrada";
-import { useState } from "react";
 import { stringParaEntradaDeData } from "@/utils/converters";
 import Botao from "./botao";
 import Dropdown from "./dropdown";
@@ -16,8 +16,25 @@ export default function Formulario(props: FormularioProps) {
   const [filmname, setNome] = useState(props.sessao?.filmname);
   const [data, setData] = useState(props.sessao?.data);
   const [descricao, setDescricao] = useState(props.sessao?.description);
-  const [status, setStatus] = useState(props.sessao?.status || 'PREVISTO');
+  const [status, setStatus] = useState(props.sessao?.status || "PREVISTO");
   const opcoesStatus = ["PREVISTO", "ABERTO", "ENCERRADO", "CANCELADO"];
+
+  const [mostrarAviso, setMostrarAviso] = useState(false);
+
+  const validarCampos = () => {
+    if (!filmname || !data || !descricao || !status) {
+      setMostrarAviso(true);
+      return false;
+    }
+    return true;
+  };
+
+  const handleSalvarClick = () => {
+    if (validarCampos()) {
+      setMostrarAviso(false);
+      props.sessaoMudou?.(new Sessao(id, filmname, data, descricao, status));
+    }
+  };
 
   return (
     <div>
@@ -26,32 +43,25 @@ export default function Formulario(props: FormularioProps) {
       ) : (
         false
       )}
-      <Entrada
-        texto="Nome do Filme"
-        valor={filmname}
-        onChange={setNome}
-      ></Entrada>
+      <Entrada texto="Nome do Filme" valor={filmname} onChange={setNome}></Entrada>
       <Entrada
         texto="Data"
         tipo="date"
         valor={stringParaEntradaDeData(data)}
         onChange={setData}
       ></Entrada>
-      <Entrada
-        texto="Descrição"
-        valor={descricao}
-        onChange={setDescricao}
-      ></Entrada>
+      <Entrada texto="Descrição" valor={descricao} onChange={setDescricao}></Entrada>
       <Dropdown options={opcoesStatus} value={status} onChange={setStatus} />
       <div className="flex justify-end mt-5">
+        {mostrarAviso && (
+          <div className="bg-red-100 border border-red-400 text-red-700 px-3 py-2 rounded mr-3">
+            Preencha todos os campos antes de salvar.
+          </div>
+        )}
         <Botao
           className="mr-3"
           cor="bg-gradient-to-r from-red-500 to-red-700"
-          onClick={() =>
-            props.sessaoMudou?.(
-              new Sessao(id, filmname, data, descricao, status)
-            )
-          }
+          onClick={handleSalvarClick}
         >
           {id ? "Alterar" : "Salvar"}
         </Botao>
