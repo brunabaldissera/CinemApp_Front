7e7586ecb7f3ac2e5b0bc1d277912ac657931258
unknown <brunabaldissera12@gmail.com>
2023-11-24 17:04:03 -0300

    alterando descricao

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div className="container">
       <div className="left-column">
         <h2 className="welcome-heading">Bem-vindo ao <span className="brand-name">CinemApp</span></h2>
-        <p className="description">Uma aplicação para controle de cinema</p>
+        <p className="description">Um aplicativo para controle de cinema</p>
       </div>
       <div className="right-column">
         <img className="cinem-logo" src="/cinem.png" alt="Cinem Logo" />
