a8cca1eb2ade79e50ee3b224f8382d36809fa568
unknown <brunabaldissera12@gmail.com>
2023-11-24 17:19:05 -0300

    alterar sessao

--- src/app/sessoes/page.tsx
+++ src/app/sessoes/page.tsx
@@ -9,9 +9,9 @@ import { atualizarSessao, cadastrarSessao, excluirSessao, fetchSessoes } from "@
 
 
 export default function Sessoes() {
-    const [sessao, setSessao] = useState<Sessao>(Sessao.vazio())
-    const [visivel, setVisivel] = useState<'tabela' | 'form'>('tabela')
-    const [sessoes, setSessoes] = useState<Sessao[]>([]);
+  const [sessao, setSessao] = useState<Sessao>(Sessao.vazio())
+  const [visivel, setVisivel] = useState<'tabela' | 'form'>('tabela')
+  const [sessoes, setSessoes] = useState<Sessao[]>([]);
 
   useEffect(() => {
     if (visivel === 'tabela') {
@@ -27,54 +27,62 @@ export default function Sessoes() {
       loadSessoes();
     }
   }, [visivel]);
-    function sessaoSelecionada(sessao: Sessao) {
-        setSessao(sessao)
-        setVisivel('form')
-    }     
-    function novaSessao() {
-        setSessao(Sessao.vazio())
-        setVisivel("form")
-    }  
-    async function sessaoExcluida(sessao: Sessao) {
-        const confirmacao =
-        window.confirm("Tem certeza de que deseja excluir esta sessão?");
-        if (confirmacao) {
-        try {
+
+  function sessaoSelecionada(sessao: Sessao) {
+    setSessao(sessao)
+    setVisivel('form')
+  }
+
+  function novaSessao() {
+    setSessao(Sessao.vazio())
+    setVisivel("form")
+  }
+
+  async function sessaoExcluida(sessao: Sessao) {
+    const confirmacao =
+      window.confirm("Tem certeza de que deseja excluir esta sessão?");
+    if (confirmacao) {
+      try {
         if (sessao.id !== null) {
-        await excluirSessao(sessao.id);
+          await excluirSessao(sessao.id);
         } else {
-        console.error("sessaoID é null!");
+          console.error("sessaoID é null!");
         }
         setSessoes(prevSessoes => prevSessoes.filter(ev => ev.id !== sessao.id));
-        } catch (error) {
+      } catch (error) {
         console.error("Erro ao excluir sessão:", error);
-        }}}
-    async function salvarSessao(sessao: Sessao) {
-        try {
-         const novaSessao = await cadastrarSessao(sessao);
-         setVisivel("tabela");
-         } catch (error) {
-         console.error("Erro ao salvar sessão:", error);
-         }
-        }
-        async function alterarSessao(sessao: Sessao) {
-            try {
-            const sessaoAtualizada = await atualizarSessao(sessao);
-            setVisivel("tabela");
-            } catch (error) {
-            console.error("Erro ao atualizar sessão:", error);
-            }
-           }
-           function salvarOuAlterarSessao(sessao: Sessao) {
-            if (sessao.id) {
-            alterarSessao(sessao)
-            } else {
-            salvarSessao(sessao)
-            }
-           }
-        
-    return (
-        <div className={`
+      }
+    }
+  }
+
+  async function salvarSessao(sessao: Sessao) {
+    try {
+      const novaSessao = await cadastrarSessao(sessao);
+      setVisivel("tabela");
+    } catch (error) {
+      console.error("Erro ao salvar sessão:", error);
+    }
+  }
+
+  async function alterarSessao(sessao: Sessao) {
+    try {
+      const sessaoAtualizada = await atualizarSessao(sessao);
+      setVisivel("tabela");
+    } catch (error) {
+      console.error("Erro ao atualizar sessão:", error);
+    }
+  }
+
+  function salvarOuAlterarSessao(sessao: Sessao) {
+    if (sessao.id) {
+      alterarSessao(sessao)
+    } else {
+      salvarSessao(sessao)
+    }
+  }
+
+  return (
+    <div className={`
                 flex justify-center items-center h-screen
                 background: linear-gradient(
                   to bottom,
@@ -83,22 +91,22 @@ export default function Sessoes() {
                   rgb(var(--background-end-dark-rgb))
                 );
         `}>
-            <Layout titulo="Cadastro de sessões">
-                {visivel === 'tabela' ? (
-                    <> <div className="flex justify-end">
-                        <Botao className="mb-4" cor="bg-gradient-to-r from-red-500 to-red-700"
-                            onClick={() => novaSessao()}>
-                            Nova Sessão </Botao>
-                    </div>
-                        <Tabela sessoes={sessoes}
-                            sessaoSelecionada={sessaoSelecionada}
-                            sessaoExcluida={sessaoExcluida}></Tabela>
-                    </>
-                ) : (<Formulario sessao={sessao}
-                    sessaoMudou={salvarSessao}
-                    cancelado={() => setVisivel('tabela')} />
-                   )}
-            </Layout>
-        </div>
-    )
+      <Layout titulo="Cadastro de sessões">
+        {visivel === 'tabela' ? (
+          <> <div className="flex justify-end">
+            <Botao className="mb-4" cor="bg-gradient-to-r from-red-500 to-red-700"
+              onClick={() => novaSessao()}>
+              Nova Sessão </Botao>
+          </div>
+            <Tabela sessoes={sessoes}
+              sessaoSelecionada={sessaoSelecionada}
+              sessaoExcluida={sessaoExcluida}></Tabela>
+          </>
+        ) : (<Formulario sessao={sessao}
+          sessaoMudou={salvarOuAlterarSessao}
+          cancelado={() => setVisivel('tabela')} />
+        )}
+      </Layout>
+    </div>
+  )
 }
